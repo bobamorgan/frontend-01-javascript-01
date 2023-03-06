a0ba008869a607870e22e00bf7bc6ea1cd3b71f4
bobamorgan <bobamorgan@yandex.ru>
2023-03-07 00:38:11 +0300

    Add files via upload

---
+++ js-info.js
@@ -0,0 +1,5 @@
+alert('Язык программирования JavaScript разработал Brendan Eich в 1995 году. Тогда он был сотрудником Netscape Communications Corporation.')
+alert('JavaScript был разработан за 10 дней.');
+alert('Разработка языка JavaScript началась в 1995 году.');
+alert('JavaScript - это не Java.');
+alert('Спецификация JavaScript называется ECMAScript.');
